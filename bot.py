64e43808964f20fcf504b196585fb8f2fb75251f
Armat Ilimisov <armat.ilimisov@gmail.com>
2020-06-01 23:22:49 +0300

    Добавил приветствие

--- bot.py
+++ bot.py
@@ -5,15 +5,21 @@ import settings
 
 
 
-def start_bot(bot, updater):
-	print("start")
+def start_bot(bot, update):
+	mytext = """Ну привет тебе, братюнь!
+
+Я простой бот и понимаю только команду /start
+	"""
+	update.message.reply_text(mytext)
+
+	print("start") #пишется "start" в командной строке. это как бы отладочная фиговина
 
 
 
 def main():
 	updtr = Updater(settings.TELEGRAM_API_KEY, use_context = True)
 
-	updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+	updtr.dispatcher.add_handler(CommandHandler("start", start_bot)) #когда жмякают start, запускается функция start_bot
 
 
 
